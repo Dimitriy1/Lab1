9467e07091aa769d38b387d30606d7ea37273f07
Dimitriy1 <dimalitvin192.0@gmail.com>
2019-01-20 19:33:54 +0200

    Update Romb.java

--- src/Romb.java
+++ src/Romb.java
@@ -4,37 +4,29 @@ public class Romb {
 
     public static void drawRomb(int numb){
         int i = numb % 2;
-
         if (i == 0){
             throw new IllegalArgumentException("even number is not supported");
         }
 
         int k = i;
         int numbOfBlanks = numb / 2;
-
         while (true){
             String blank = "";
-
             for (int j = 0; j < numbOfBlanks; j++) {
                 blank += " ";
             }
-
             for (int j = 1; j <= i ; j++) {
                 blank += "*";
             }
-
             System.out.print(blank + "\n");
-
-            if (k < numb){
+            if (k < numb) {
                 numbOfBlanks--;
                 i += 2;
                 k = i;
-            }
-            else {
+            } else {
                 numbOfBlanks++;
                 i -= 2;
             }
-
             if (i < numb % 2){
                 break;
             }
